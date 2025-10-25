aa45707fee7fcf0cc1c7b9a9fbb5e4acb268a25b
OxBryte <silascyrax@gmail.com>
2025-10-25 01:30:59 +0100

    feat(api): update base URL configuration to use environment variable
    - Replaced hardcoded base URL with dynamic API_URL from environment variables for improved flexibility and deployment configuration.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+export const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5858/api",
+  baseURL: `${API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
